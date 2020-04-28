e43ccee5872278ca1d06ce479419056ac0a0ba68
Ilza <ilzammed2@gmail.com>
2020-04-28 18:24:06 -0300

    To Do List finalizada com os requisitos descritos no README.md

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     //adicionando itens na lista
-   const input = document.querySelector('#todoInput')
-   const lista = document.querySelector('#todoLista')
-   const addButton = document.querySelector('#todoSubmit')
+    const input = document.querySelector('#todoInput')
+    const lista = document.querySelector('#todoLista')
+    const addButton = document.querySelector('#todoSubmit')
+    const buttonMarcarTodos = document.querySelector('#todoMarcarTodos')
+    const buttonRemoveTodos = document.querySelector('#todoRemoverTodos')
 
-   let addItemOnList = (event) => {
-       event.preventDefault()
-       
-       //checando se é isBlanc
-    if(input.value == null || input.value == ''){
-        alert(`Insira uma palavra válida`)
-    } else {
-        //cria o item na lista
-       const item = document.createElement('li')
-       item.innerText = input.value
-       lista.appendChild(item)
-       
-        //cria o botão do item para excluir
-       const miniButton = document.createElement('button')
-       miniButton.innerText = 'x'
-       item.appendChild(miniButton)
-
-       miniButton.addEventListener('click', () => {
-           lista.removeChild(item)
-       })
+    
+    
+    let addItemOnList = (event) => {
+        event.preventDefault()
         
-        //Marcar itens concluídos individualmente
-        item.addEventListener('click', () => {
-            item.style.textDecoration = 'line-through'
-           })
+        //checando se é isBlanc
+        if (input.value == null || input.value == '') {
+            alert(`Insira uma palavra válida`)
+        } else {
+            
+            //cria uma div que vai receber o item e o botão
+            const div = document.createElement('div')
+            const item = document.createElement('li')
+            const miniButton = document.createElement('button')
+            
+            //Adiciona o valor no item o 'x' do botão
+            item.innerText = input.value
+            miniButton.innerText = 'x'
+            
+            //adiciona o item e o botão na div
+            div.appendChild(item)
+            div.appendChild(miniButton)
+            
+            //adiciona a div na lista(ul)
+            lista.appendChild(div)
+
+            //adicionando display flex na div
+            div.style.display = 'flex'
+            div.style.justifyContent = 'space-between'
+            div.style.border = '1px solid #ccc'
+        
+            
+            // EVENTOS
+            miniButton.addEventListener('click', () => {
+                lista.removeChild(div)
+            })
+            
+            //Marcar itens concluídos individualmente
+            item.addEventListener('click', () => {
+                item.style.textDecoration = 'line-through'
+            })
+        }
     }
-  
+    
+    addButton.addEventListener('click', addItemOnList)
 
+    //Deixando a caixa de texto vazia quando o usuário incluir um valor e apertar o botão ADD
+    addButton.addEventListener('click', () => {
+        document.querySelector('input').value = ''
+    })
 
-   }
-
-   addButton.addEventListener('click', addItemOnList)
-  
-  
-
-
-
+    
 })
